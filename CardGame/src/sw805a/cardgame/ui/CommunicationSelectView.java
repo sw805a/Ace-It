1541fd4a744b9cded7b263b2d5f9730e037c6b77
Torben <git@plazm.dk>
2011-06-19 15:04:01 +0200

    Menus added

---
+++ CardGame/src/sw805a/cardgame/ui/CommunicationSelectView.java
@@ -0,0 +1,87 @@
+package sw805a.cardgame.ui;
+
+import sw805a.cardgame.ApplicationController;
+import sw805a.cardgame.R;
+import sw805a.cardgame.comm.CommunicationType;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CommunicationSelectView extends ABaseActivity {
+   
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.comm_select);
+        
+        Button bluetooth = (Button)findViewById(R.id.bluetooth);
+        bluetooth.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				getGameEngine().setCommunicationType(CommunicationType.BLUETOOTH);
+				startActivity(new Intent(CommunicationSelectView.this, GameSelectView.class));
+			}
+		});
+        
+        Button internet = (Button)findViewById(R.id.internet);
+        internet.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				getGameEngine().setCommunicationType(CommunicationType.INTERNET);
+				startActivity(new Intent(CommunicationSelectView.this, GameSelectView.class));
+			}
+		});
+        
+        Button settings = (Button)findViewById(R.id.settings);
+        settings.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(CommunicationSelectView.this);
+
+				alert.setTitle("Title");
+				alert.setMessage("Message");
+
+				// Set an EditText view to get user input 
+				final EditText input = new EditText(CommunicationSelectView.this);
+				alert.setView(input);
+
+
+				alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					String value = input.getText().toString();
+				  	SharedPreferences prefs = ApplicationController.getContext().getSharedPreferences("CardGame", 0);
+					SharedPreferences.Editor editor = prefs.edit();
+					editor.putString("ip", value);
+					editor.commit();
+				  }
+				});
+
+				alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				  public void onClick(DialogInterface dialog, int whichButton) {
+				    // Canceled.
+				  }
+				});
+
+				alert.show();
+			}
+		});
+	}
+	
+	@Override
+	public void onResume() {
+        super.onResume();
+	}
+	
+	@Override
+	protected void onPause() {
+		
+        super.onPause();
+	}
+}
